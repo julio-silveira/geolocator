c1941683b261b498685e47ac47475a10aa9ded40
julio-silveira <julioqueiroz15@gmail.com>
2024-03-24 20:30:04 -0300

    feature: swagger ui

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,17 @@
 import 'express-async-errors'
 
 import express from 'express'
+
 import router from './routes'
+import swaggerUi from 'swagger-ui-express';
 import errorMiddleware from './middleware/errors'
 const app = express()
+import swaggerDocs from './swagger.json';
 
 app.use(express.json())
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 app.use(errorMiddleware)
 
